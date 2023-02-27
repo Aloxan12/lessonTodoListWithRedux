c9b98e606b7144c070fbf6566f4c1fa53e938ba8
Aloxan12 <kvakvaaak@mail.ru>
2023-02-27 08:53:05 +0300

    pages Calculator

---
+++ src/components/MainLayout/MainLayout.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import './MainLayout.css'
+
+export const MainLayout = () => {
+    return <Outlet />
+}
--- src/router.tsx
+++ src/router.tsx
@@ -1,32 +1,36 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import {BrowserRouter, Route, Routes} from "react-router-dom"
 import {App} from "./pages/Main/App";
 import {About} from "./pages/About/About";
 import {Calculator} from "./pages/Calculator/Calculator";
+import {Header} from "./components/Header/Header";
+import {MainLayout} from "./components/MainLayout/MainLayout";
 
 const routes = [
     {
         id: 'Home',
         path: '/',
-        component: <App />
+        component: <App/>
     },
     {
         id: 'About',
         path: '/about',
-        component: <About />
+        component: <About/>
     },
     {
         id: 'Calculator',
         path: '/calculator',
-        component: <Calculator />
+        component: <Calculator/>
     }
 ]
 
-export const AppRouter = ()=>{
+export const AppRouter = () => {
     return <BrowserRouter>
+        <Header/>
+        <MainLayout/>
         <Routes>
             {routes
-                .map(({id, path, component})=>
-                    <Route key={id} path={path} element={component} />
+                .map(({id, path, component}) =>
+                    <Route key={id} path={path} element={component}/>
                 )}
         </Routes>
     </BrowserRouter>
