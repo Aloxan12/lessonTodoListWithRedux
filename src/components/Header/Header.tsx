c5cd7480b2fd6ed180d38fbe76f9332dbc485ac6
Aloxan12 <kvakvaaak@mail.ru>
2023-02-28 11:05:26 +0300

    pages Calculator

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
-import './Header.css';
+import { NavLink } from 'react-router-dom';
+import './Header.scss';
 
 export const Header = () => {
     return (
-        <header className={'header-main'}>
-            Header
+        <header className='header-main'>
+            <div className="title">Натяжные потолки</div>
+            <nav className='nav'>
+                <NavLink to=''>Главная</NavLink>
+                <NavLink to=''>Калькулятор</NavLink>
+                <NavLink to=''>Мои Работы</NavLink>
+            </nav>
         </header>
     );
 };
