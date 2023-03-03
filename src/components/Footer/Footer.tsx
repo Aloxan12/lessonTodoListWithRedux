85fd72d6e81cd16287ac164da078ed3efb38346b
Aloxan12 <kvakvaaak@mail.ru>
2023-03-03 16:57:07 +0300

    mixin

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import './Footer.scss'
+import telegram from '../../utils/icons/ico-telegram.png'
+import instagram from '../../utils/icons/instagram.png'
+import phone from '../../utils/icons/phone.png'
+import viber from '../../utils/icons/viber.png'
 
 export const Footer = () => {
     return (
         <div className='footer-wrap'>
             <div className='container'>
-                Footer
+                <div className='footer-block'>
+                    <a href="#" className='footer-link'><img src={telegram} alt='telegram'/></a>
+                    <a href="#" className='footer-link'><img src={instagram} alt='instagram'/></a>
+                    <a href="#" className='footer-link'><img src={phone} alt='phone'/></a>
+                    <a href="#" className='footer-link'><img src={viber} alt='viber'/></a>
+                </div>
             </div>
         </div>
     );
