e89ebebf76e3eedb51d72d1a4c47c41197964773
Aloxan12 <kvakvaaak@mail.ru>
2023-03-06 13:33:33 +0300

    Calculator page

--- src/pages/Calculator/Calculator.tsx
+++ src/pages/Calculator/Calculator.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import './Calculator.scss'
 
 export const Calculator = () => {
+
     return (
-        <div>
-            Calculator
+        <div className='calculator-wrap'>
+            <CalculatorTitle />
         </div>
     );
 };
+
+const CalculatorTitle = React.memo(()=>{
+    return <div className='calculator-wrap'>
+        <h1>Калькулятор</h1>
+        <h3>Здесь вы можете расчитать примерную смоимость.</h3>
+    </div>
+})
+
