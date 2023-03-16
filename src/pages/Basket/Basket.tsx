74d971a4f675c121f7622aea00ff3ee1193f8c82
Aloxan12 <kvakvaaak@mail.ru>
2023-03-16 14:41:26 +0300

    header

--- src/pages/Basket/Basket.tsx
+++ src/pages/Basket/Basket.tsx
@@ -1,6 +1,8 @@
 import React, { useMemo } from "react";
 import { AppTitlePage } from "../../components/AppTitlePage/AppTitlePage";
 import { getRoomsArr } from "../../helpers/getRoomsCount";
+import "./Basket.scss";
+import { IRoom } from "../Calculator/components/RoomForm";
 
 export const Basket = () => {
   const rooms = useMemo(() => getRoomsArr(), [localStorage]);
@@ -8,7 +10,17 @@ export const Basket = () => {
   return (
     <div className="calculator-wrap">
       <AppTitlePage title="Корзина" />
-      <div></div>
+      <div className="basket-rooms-wrap">
+        {!!rooms.length ? (
+          rooms.map((item: IRoom, index: number) => (
+            <div key={`${item.id} ${index}`}>{item.title}</div>
+          ))
+        ) : (
+          <div className="empty-block">
+            <span>Помещений нет</span>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
