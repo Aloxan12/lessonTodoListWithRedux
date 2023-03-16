5286414868c11bc0e47ab9ae4a61b728434cb265
Aloxan12 <kvakvaaak@mail.ru>
2023-03-16 14:32:36 +0300

    header

--- src/pages/Basket/Basket.tsx
+++ src/pages/Basket/Basket.tsx
@@ -1,5 +1,14 @@
-import React from "react";
+import React, { useMemo } from "react";
+import { AppTitlePage } from "../../components/AppTitlePage/AppTitlePage";
+import { getRoomsArr } from "../../helpers/getRoomsCount";
 
 export const Basket = () => {
-  return <div></div>;
+  const rooms = useMemo(() => getRoomsArr(), [localStorage]);
+
+  return (
+    <div className="calculator-wrap">
+      <AppTitlePage title="Корзина" />
+      <div></div>
+    </div>
+  );
 };
